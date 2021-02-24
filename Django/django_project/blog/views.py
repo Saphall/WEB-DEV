f07a0ffbd8e366232c911c5d5c8c13b092c9c84c
Saphall <safalsakha@gmail.com>
2021-02-24 10:03:14 +0545

    commit

--- Django/django_project/blog/views.py
+++ Django/django_project/blog/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1> Hello this is blog home.</h1>')
+    return HttpResponse('<h1> Hello this is blog home.We are writing other things in templete now.</h1>')
 
 def about(request):
     return HttpResponse('<h1>This is blog about page.</h1>')
