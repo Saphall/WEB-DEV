98b63c00ee1288ddde11222e709dbd6e2f6611e2
Saphal Shakha <66344649+Saphall@users.noreply.github.com>
2023-01-12 07:40:17 +0545

    Update app.py

--- Flask/0Flask_Basics/app.py
+++ Flask/0Flask_Basics/app.py
@@ -5,13 +5,11 @@ from flask import Flask
 app = Flask(__name__)
 
 # 3. Set route
-
-
 @app.route('/')
+
 # 4. your controller function
 def index():
     return 'Hello Flask.'
-
 
 # 5. run main program
 if __name__ == "__main__":
